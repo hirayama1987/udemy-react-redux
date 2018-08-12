4a06df68a6f6f6726c91473d107932a8e8e9ad77
hirayama1987 <naoki.hirayama1987@gmail.com>
2018-08-12 16:57:19 +0900

    mew

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-      <input type="text" onClick={()=>{console.log("クリック")}}/>
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//       <input type="text" onClick={()=>{console.log("クリック")}}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+    </div>
+  )
 }
+
+const Cat = () => {
+  return <div>mewewe</div>;
+}
+
 
 export default App;
