50f27006df9e19cc6691d855628eba059cf41505
hirayama1987 <naoki.hirayama1987@gmail.com>
2018-08-12 17:26:58 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // class App extends Component {
 //   render() {
@@ -13,7 +14,8 @@ import React, { Component } from 'react';
 const App = () => {
   const profiles = [
     {name:"taro",age:10},
-    {name:"aaa"}
+    {name:"aaa",age:10},
+    {name:"aaa",age:12}
   ]
   return (
     <div>
@@ -29,8 +31,9 @@ const App = () => {
 const User = (props) => {
   return <div>Hi, I am {props.name},and {props.age} years old.</div>;
 }
-User.defaultProps = {
-  age:1
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired
 }
 
 
